32ba06fd25b3338658a5af2aa36d3b6d0a101a36
Aliaksandr Hapankou <akkforrs@gmail.com>
2022-08-12 20:06:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str.split('');
+  for (let i = 0; i < newStr.length; i++) {
+    bracketsConfig.forEach((el) => {
+      if (el[0].indexOf(newStr[i]) >= 0 && el[1].indexOf(newStr[i + 1]) >= 0) {
+       newStr.splice(i, 2);
+       i = -1;
+      };
+    });
+  };
+  return newStr.length === 0 ? true : false;// your solution
 }
